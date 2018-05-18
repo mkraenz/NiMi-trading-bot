64c96f485d977594f5df365af4a971944c522859
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-18 20:28:52 +0900

    #8 ADD TEST for InvestopediaApi

---
+++ src/test/test.py
@@ -0,0 +1,13 @@
+'''
+Created on 11.05.2018
+
+@author: Mirco
+'''
+
+print("Hello World!")
+
+from test_investopedia_trading_api.test_investopedia_trading_api import TestInvestopediaApi
+
+TestInvestopediaApi().test_login()
+
+    
