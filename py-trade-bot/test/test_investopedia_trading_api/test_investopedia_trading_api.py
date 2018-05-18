999ea5c93f01bc43e6ccd3bae15443d0127b5432
Mirco Kraenz <prosingularity@yahoo.de>
2018-05-18 20:22:19 +0900

    #8 ADD TEST for InvestopediaApi

--- py-trade-bot/test/test_investopedia_trading_api/test_investopedia_trading_api.py
+++ py-trade-bot/test/test_investopedia_trading_api/test_investopedia_trading_api.py
@@ -4,39 +4,41 @@ Created on 11.05.2018
 @author: Mirco
 '''
 import unittest
-from InvestopediaApi import ita
 from test_investopedia_trading_api.config import TEST_PASSWORD, TEST_USERNAME
-from InvestopediaApi.ita import Action
+from InvestopediaApi.ita import Action, get_quote, Account
 
 
 class TestInvestopediaApi(unittest.TestCase):
     
-    STOCK_SYMBOL = "NVCN"
+    SYMBOL = "NVCN"
     PASSWORD = TEST_PASSWORD
     USERNAME = TEST_USERNAME
     
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.client = ita.Account(self.USERNAME, self.PASSWORD)
+        self.client = Account(self.USERNAME, self.PASSWORD)
 
     def test_login(self):
-        client = ita.Account(self.USERNAME, self.PASSWORD)
+        client = Account(self.USERNAME, self.PASSWORD)
         self.assertTrue(client.logged_in)
 
     def test_buy(self):
-        self.assertTrue(self.client.trade(self.STOCK_SYMBOL, Action.buy, 1))
+        self.assertTrue(self.client.trade(self.SYMBOL, Action.buy, 1))
 
     def test_sell(self):
         portfolio = self.client.get_current_securities()
         for bought in portfolio.bought:
-            if bought.symbol == self.STOCK_SYMBOL:
-                self.assertTrue(self.client.trade(self.STOCK_SYMBOL, Action.sell, 1))
+            if bought.symbol == self.SYMBOL:
+                self.assertTrue(self.client.trade(self.SYMBOL, Action.sell, 1))
                 return
         self.assertTrue(False,
                             'Possesses no stock to test TestInvestopediaApi.test_sell(). Buy some manually.')
 
     def test_get_current_securities(self):
         self.assertTrue(self.client.get_current_securities())
+        
+    def test_get_quote(self):
+        self.assertTrue(get_quote(self.SYMBOL))
         
 if __name__ == "__main__":
     unittest.main()
