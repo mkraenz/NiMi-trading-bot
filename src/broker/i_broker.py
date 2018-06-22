52344a7f602de7899b382200cb9f0b8f0a8ac93d
Mirco Kraenz <prosingularity@yahoo.de>
2018-06-22 17:58:55 +0900

    #12 ADD interfaces for abstraction from api

---
+++ src/broker/i_broker.py
@@ -0,0 +1,22 @@
+class IBroker(object):
+    
+    def bid(self, symbol):
+        raise NotImplementedError("Class %s doesn't implement bid()" % (self.__class__.__name__))
+
+    def ask(self, symbol):
+        raise NotImplementedError("Class %s doesn't implement ask()" % (self.__class__.__name__))
+    
+    def fees(self, symbol):
+        raise NotImplementedError("Class %s doesn't implement fees()" % (self.__class__.__name__))
+
+    def stocks(self):
+        raise NotImplementedError("Class %s doesn't implement stocks()" % (self.__class__.__name__))
+
+    def cash(self):
+        raise NotImplementedError("Class %s doesn't implement cash()" % (self.__class__.__name__))
+    
+    def portfolio_value(self):
+        raise NotImplementedError("Class %s doesn't implement posrtfolio_value()" % (self.__class__.__name__))
+
+    def login(self):
+        raise NotImplementedError("Class %s doesn't implement login()" % (self.__class__.__name__))
