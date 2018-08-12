871c930fdcdee20b323335baf1e7eafcc592720d
Mirco Kraenz <kraenzmirco@gmail.com>
2018-08-12 15:44:04 +0900

    #14 RENAME client to broker in `TestInvestopediaApi`

--- test/test_investopedia_trading_api/test_investopedia_trading_api.py
+++ test/test_investopedia_trading_api/test_investopedia_trading_api.py
@@ -16,26 +16,26 @@ class TestInvestopediaApi(unittest.TestCase):
     
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.client = Account(self.USERNAME, self.PASSWORD)
+        self.broker = Account(self.USERNAME, self.PASSWORD)
 
     def test_login(self):
         client = Account(self.USERNAME, self.PASSWORD)
         self.assertTrue(client.logged_in)
 
     def test_buy(self):
-        self.assertTrue(self.client.trade(self.SYMBOL, Action.buy, 1))
+        self.assertTrue(self.broker.trade(self.SYMBOL, Action.buy, 1))
 
     def test_sell(self):
-        portfolio = self.client.get_current_securities()
+        portfolio = self.broker.get_current_securities()
         for bought in portfolio.bought:
             if bought.symbol == self.SYMBOL:
-                self.assertTrue(self.client.trade(self.SYMBOL, Action.sell, 1))
+                self.assertTrue(self.broker.trade(self.SYMBOL, Action.sell, 1))
                 return
         self.assertTrue(False,
                             'Possesses no stock to test TestInvestopediaApi.test_sell(). Buy some manually.')
 
     def test_get_current_securities(self):
-        self.assertTrue(self.client.get_current_securities())
+        self.assertTrue(self.broker.get_current_securities())
         
     def test_get_quote(self):
         self.assertTrue(get_quote(self.SYMBOL))
